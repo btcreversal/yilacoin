e08dc8c2225061de83b7bc60939177b9159b20ed
Jay Bitron <jaybitron@gmail.com>
2022-02-06 01:13:56 +0330

    fix reg key bug

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -42,12 +42,12 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_SIGNET "Bitcoin-Qt-signet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_ORG_NAME "Yilacoin"
+#define QAPP_ORG_DOMAIN "yilacoin.eu.org"
+#define QAPP_APP_NAME_DEFAULT "Yilacoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Yilacoin-Qt-testnet"
+#define QAPP_APP_NAME_SIGNET "Yilacoin-Qt-signet"
+#define QAPP_APP_NAME_REGTEST "Yilacoin-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
